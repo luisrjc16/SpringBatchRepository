4cf60b43c7e8dee5b470ee9d65db9d70ae557e0d
Luis Jaime Calva <lrjc2508@gmail.com>
2019-12-02 20:32:52 -0600

    Se elimina el archivo schema-all.sql

--- demo-spring-batch/src/main/resources/schema-all.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE persona IF EXISTS;
-
-CREATE TABLE persona(
-  id BIGINT IDENTITY NOT NULL PRIMARY KEY,
-  primer_nombre VARCHAR(20),
-  segundo_nombre VARCHAR(20),
-  telefono VARCHAR(10)
-);
