710e4bc21900242b064dd83ae1feeffabfe7fe8c
Luis Rey Jaime Calva <jaimelr@globalhitss.com>
2019-12-02 18:38:19 -0600

    Se agrega Lombok al proyecto

--- src/main/java/com/example/demospringbatch/model/Persona.java
+++ src/main/java/com/example/demospringbatch/model/Persona.java
@@ -1,49 +1,15 @@
 package com.example.demospringbatch.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data @AllArgsConstructor @NoArgsConstructor @ToString
 public class Persona {
 
 	private String primerNombre;
 	private String segundoNombre;
 	private String telefono;
 
-	public Persona() {
-		super();
-	}
-
-	public Persona(String primerNombre, String segundoNombre, String telefono) {
-		super();
-		this.primerNombre = primerNombre;
-		this.segundoNombre = segundoNombre;
-		this.telefono = telefono;
-	}
-
-	public String getPrimerNombre() {
-		return primerNombre;
-	}
-
-	public void setPrimerNombre(String primerNombre) {
-		this.primerNombre = primerNombre;
-	}
-
-	public String getSegundoNombre() {
-		return segundoNombre;
-	}
-
-	public void setSegundoNombre(String segundoNombre) {
-		this.segundoNombre = segundoNombre;
-	}
-
-	public String getTelefono() {
-		return telefono;
-	}
-
-	public void setTelefono(String telefono) {
-		this.telefono = telefono;
-	}
-
-	@Override
-	public String toString() {
-		return "Persona [primerNombre=" + primerNombre + ", segundoNombre=" + segundoNombre + ", telefono=" + telefono
-				+ "]";
-	}
 }
